9d9c3f8d774187b03a457d2bfffd20293347839a
KamalLahloh <kamal.lahloh@gmail.com>
2024-02-10 22:13:02 +0300

    initial commit routes.js files

---
+++ backend/routes/comments.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+//controllers
+// const {} = require("../controllers/comments");
+
+const commentsRouter = express.Router();
+
+// usersRouter.post("/", createNewRole);
+
+module.exports = commentsRouter;
