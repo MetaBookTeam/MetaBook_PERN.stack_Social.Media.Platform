521bf1abdcbe5736881391282a65b69096028122
nassarahmad <ahmad.m.nassarr@gmail.com>
2024-02-10 21:36:06 +0300

    create function createcomments

---
+++ backend/controllers/Comments.js
@@ -0,0 +1,11 @@
+import pool from "../models/db"
+//dont forget to import pool
+//create comments
+const CreateComments=(req,res)=>{
+ //endpoint and method (post,/CreateComment
+
+ //we want to create comment in req.body
+ const {comment}=req.body;
+pool.query(``,)
+}
+export default CreateComments
