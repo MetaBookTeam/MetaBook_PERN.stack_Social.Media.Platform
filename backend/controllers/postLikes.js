18ecc11cad69547846fbf2ae474b51a2c3594e3e
RafatBSaqqa <raafat.saqqa@gmail.com>
2024-02-12 21:50:11 +0300

    added delete like

--- backend/controllers/postLikes.js
+++ backend/controllers/postLikes.js
@@ -22,9 +22,30 @@ const createNewPostLike = async (req, res) => {
     })
   }
 };
-
-
+const deletePostLikeById = async (req, res) => {
+    const user_id = req.token.userId;
+    const { postId } = req.params;
+    const placeholder = [postId, user_id];
+    try {
+      const deleteLike = await pool.query(
+        `DELETE FROM posts_likes
+          WHERE id=$1 AND user_id=$2  RETURNING *`,
+        placeholder
+      );
+      res.status(200).json({
+          success:true,
+          message:"Remove like successfully",
+          res:deleteLike.rows
+      })
+    } catch (error) {
+      res.status(404).json({
+          success:false,
+          message:"Server error",
+          res:error
+      })
+    }
+};
 module.exports = {
   createNewPostLike,
-
+  deletePostLikeById,
 };
