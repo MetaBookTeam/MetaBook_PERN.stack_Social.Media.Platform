cfab5e4fa02383eeeb10d3119da701da56b81e2f
nassarahmad <ahmad.m.nassarr@gmail.com>
2024-02-16 18:59:03 +0300

    update on sql shares

--- backend/controllers/Shares.js
+++ backend/controllers/Shares.js
@@ -1,81 +1,78 @@
 const pool = require("../models/db");
 
-const getShareByPostId = (req, res) => {};
-const { post_id } = req.params;
-pool
-  .query(
-    `SELECT shares.id AS share_id,
-shares.user_id AS share_user_id,
-shares.post_id,
-posts.user_id AS post_user_id,
-posts.page_id,
-posts.content,
-posts.photo_url,
-posts.created_at,
-posts.is_deleted
+const getShareByPostId = (req, res) => {
+  const { post_id } = req.params;
+  pool
+    .query(
+      `SELECT * 
 FROM shares
 INNER JOIN posts ON shares.post_id = posts.id
 WHERE shares.post_id = $1;`,
-    [post_id]
-  )
-  .then((result) => {
-    res.status(200).json({
-      success: true,
-      message: "get share succesfully",
-      result: result.rows,
+      [post_id]
+    )
+    .then((result) => {
+      res.status(200).json({
+        success: true,
+        message: "get share succesfully",
+        result: result.rows,
+      });
+    })
+    .catch((error) => {
+      res.status(500).json({
+        success: false,
+        message: "Server Error",
+        errorr: errorrr.message,
+      });
     });
-  })
-  .catch((error) => {
-    res.status(500).json({
-      success: false,
-      message: "Server Error",
-      errorr: errorrr.message,
-    });
-  });
-
-const createShareByPostId = (req, res) => {};
-//const {post_id}=req.params;
-const { user_id } = req.token.id;
-pool
-  .query(
-    ` INSERT INTO shares (post_id, user_id)
+};
+const createShareByPostId = (req, res) => {
+  const { post_id } = req.params;
+  const { user_id } = req.token.id;
+  const{content}=req.body
+  pool
+    .query(
+      ` INSERT INTO shares (post_id, user_id)
 VALUES ($1, $2)
 RETURNING id;`,
-    [user_id]
-  )
-  .then((result) => {
-    res.status(200).json({
-      success: true,
-      message: "create share succesfully",
-      result: result.rows,
+      [user_id]
+    )
+    .then((result) => {
+      res.status(200).json({
+        success: true,
+        message: "create share succesfully",
+        result: result.rows,
+      });
+    })
+    .catch((error) => {
+      res.status(500).json({
+        success: false,
+        message: "Server Error",
+        error: error.message,
+      });
     });
-  })
-  .catch((error) => {
-    res.status(500).json({
-      success: false,
-      message: "Server Error",
-      error: error.message,
-    });
-  });
+};
 const softDeleteShare = (req, res) => {
-    const{share_id}=req.token.id
-  pool.query(
-    ` UPDATE shares
+  const { share_id } = req.token.id;
+  pool
+    .query(
+      ` UPDATE shares
     SET is_deleted = true
     WHERE id = $1;`,
-    [share_id]
-  ).then((result)=>{
-    res.status(200).json({
+      [share_id]
+    )
+    .then((result) => {
+      res.status(200).json({
         success: true,
         message: "delete share succesfully",
         result: result.rows,
       });
-  }).catch((error)=>{
-    res.status(500).json({
+    })
+    .catch((error) => {
+      res.status(500).json({
         success: false,
         message: "Server Error",
         error: error,
       });
-  })
+    });
 };
 module.exports = { getShareByPostId, createShareByPostId, softDeleteShare };
