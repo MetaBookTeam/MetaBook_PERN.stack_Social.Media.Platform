141031c534477b7a6d6996a0b991481dbab8f0a5
KamalLahloh <kamal.lahloh@gmail.com>
2024-02-19 17:47:22 +0300

    Login working + put posts page error

--- frontend/src/Service/api/posts.js
+++ frontend/src/Service/api/posts.js
@@ -1,17 +1,36 @@
 import axios from "axios";
 
-// data loader
+import { useSelector } from "react-redux";
 
 export const postsLoader = async () => {
-  // handling the promise async/await will be inside the <Suspense><Await> see above.
-  //! don't put await here or the useLoaderData will block the return until the promise response received.
+  const auth = useSelector((state) => state.auth);
+  console.log(auth);
 
-  const results = axios.get("http://localhost:5000/posts/");
-  //* if I don't want to return the whole results I'll use .then()
-  //   .then((response) => {
-  //     return response.data.result; //! this return an array not object ==> change from results.data.result.map() to results.map()
-  //   });
+  const results = axios.get("http://localhost:5000/posts/", {
+    headers: {
+      Authorization: `Bearer ${auth.token}`,
+    },
+  });
 
-  // return an object with results
   return { results };
 };
+
+// export const postsLoader = async () => {
+//   const auth = useSelector((state) => state.auth);
+//   console.log(auth);
+//   // handling the promise async/await will be inside the <Suspense><Await> see above.
+//   //! don't put await here or the useLoaderData will block the return until the promise response received.
+
+//   const results = axios.get("http://localhost:5000/posts/", {
+//     headers: {
+//       Authorization: `Bearer ${auth.token}`,
+//     },
+//   });
+//   //* if I don't want to return the whole results I'll use .then()
+//   //   .then((response) => {
+//   //     return response.data.result; //! this return an array not object ==> change from results.data.result.map() to results.map()
+//   //   });
+
+//   // return an object with results
+//   return { results };
+// };
