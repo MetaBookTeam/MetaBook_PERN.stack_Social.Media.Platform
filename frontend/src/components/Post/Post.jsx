068012c9768d7b6d29b1478b13bb871e1b6fe1fd
KamalLahloh <kamal.lahloh@gmail.com>
2024-02-19 22:28:13 +0300

    remove loader and fetch data inside useEffect

--- frontend/src/components/Post/Post.jsx
+++ frontend/src/components/Post/Post.jsx
@@ -1,14 +1,12 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-// import useDispatch and useSelector to dispatch and subscribe to the store
+import { useState } from "react";
+
 import { useDispatch, useSelector } from "react-redux";
-// import the actions
-import { setPosts, addPost } from "../../Service/redux/reducers/Posts/postsSlice";
-
-
+import {
+  setPosts,
+  addPost,
+} from "../../Service/redux/reducers/Posts/postsSlice";
 
 const Post = () => {
-
   // useDispatch allows us to dispatch actions to the reducers
   const dispatch = useDispatch();
   // useSelector gives us access to the store
@@ -18,20 +16,20 @@ const Post = () => {
     };
   });
 
-  const [Post, setPost] = useState("")
-  return(
-  <>
-  <div className="post">Post
-  <input type="text"onChange={(e)=>{
-    setPost(e.target.value)
-  }}/>
- 
- 
-  </div>;
-
-
-  </>
-  )
+  const [Post, setPost] = useState("");
+  return (
+    <>
+      <div className="post">
+        Post
+        <input
+          type="text"
+          onChange={(e) => {
+            setPost(e.target.value);
+          }}
+        />
+      </div>
+    </>
+  );
 };
 
 export default Post;
--- frontend/src/pages/Posts/Posts.jsx
+++ frontend/src/pages/Posts/Posts.jsx
@@ -1,47 +1,85 @@
-import React, { Suspense } from "react";
-import { useLoaderData, Await } from "react-router-dom";
+import { useEffect, useState } from "react";
 import axios from "axios";
+
+import { useDispatch, useSelector } from "react-redux";
+import {
+  setPosts,
+  addPost,
+} from "../../Service/redux/reducers/Posts/postsSlice";
+
 import Post from "../../components/Post/Post";
 
 export default function Posts() {
-  // destructuring the results object
-  const { results } = useLoaderData();
-  
-  /* Render the data when it is available in loader 
- From Routers/index.jsx...
-  {
-    path: "/posts",
-    element: <Posts />,
-    // loader is a function to retrieve a data
-    loader: postsLoader,
-  }, */
+  // const { results } = useLoaderData();
+
+  //* Redux
+  const dispatch = useDispatch();
+
+  const { auth, posts } = useSelector((state) => {
+    return { auth: state.auth, posts: state.posts.posts };
+  });
+  // console.log("auth ======>", auth);
+  // auth.isLoggedIn, auth.token, auth.userId;
+  // console.log("all posts ======> ", posts);
+  // all posts;
+
+  const [message, setMessage] = useState("");
+  const [status, setStatus] = useState(false);
+
+  //===============================================================
+  const getAllPosts = () => {
+    axios
+      .get("http://localhost:5000/posts", {
+        headers: {
+          Authorization: `Bearer ${auth.token}`,
+        },
+      })
+      .then((results) => {
+        if (results.data.success) {
+          setStatus(true);
+          // setMessage(results.data.message);
+          dispatch(setPosts(results.data.result));
+        } else throw Error;
+      })
+      .catch((error) => {
+        if (!error.response.data.success) {
+          return setMessage(error.response.data.message);
+        }
+        setMessage("Error happened while Get Data, please try again");
+      });
+  };
+
+  //===============================================================
+
+  useEffect(() => {
+    getAllPosts();
+  }, []);
 
   return (
     <div className="posts">
       <h2>POSTS</h2>
+      {status
+        ? message && <div className="SuccessMessage">{message}</div>
+        : message && <div className="ErrorMessage">{message}</div>}
+
       <p>What's on your mind</p>
 
-      <Suspense fallback={<p>Loading data...</p>}>
-        <Await resolve={results} errorElement={<p>Error loading data</p>}>
-          {(results) => {
-            console.log(results.data.result);
-            //! Still not working even though I receive the results here
-            return (
-              <div className="post">
-                {results.data.result &&
-                  results.data.result.map((post) => {
-                    return (
-                      <div key={post.id}>
-                        <h3>post {post.id}</h3>
-                        <Post post={post} />;
-                      </div>
-                    );
-                  })}
-              </div>
-            );
-          }}
-        </Await>
-      </Suspense>
+      {/* {(results) => {
+        console.log(results.data.result);
+        return (
+          <div className="post">
+            {results.data.result &&
+              results.data.result.map((post) => {
+                return (
+                  <div key={post.id}>
+                    <h3>post {post.id}</h3>
+                    <Post post={post} />;
+                  </div>
+                );
+              })}
+          </div>
+        );
+      }} */}
     </div>
   );
 }
