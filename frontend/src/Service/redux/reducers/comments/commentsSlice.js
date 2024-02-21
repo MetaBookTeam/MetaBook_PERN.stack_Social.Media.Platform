df7864cc66a51a518e7b1a88644070c2add13900
nassarahmad <ahmad.m.nassarr@gmail.com>
2024-02-21 22:09:54 +0300

    add commentlikeandshares

--- frontend/src/Service/redux/reducers/comments/commentsSlice.js
+++ frontend/src/Service/redux/reducers/comments/commentsSlice.js
@@ -4,19 +4,22 @@ import axios from "axios";
 const commentsSlice = createSlice({
   name: "comments",
   initialState: {
-    comments: [],commentLike:[],shares:[], loading: false,
-    error: null
+    comments: [],
+    commentLike: [],
+    shares: [],
+    loading: false,
+    error: null,
   },
 
   reducers: {
-     setLoading: (state) => {
-    state.loading = true;
-    state.error = null;
-  },
-  setError: (state, action) => {
-    state.loading = false;
-    state.error = action.payload;
-  },
+    setLoading: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    setError: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
     setcomments: (state, action) => {
       state.comments = action.payload;
     },
@@ -24,28 +27,68 @@ const commentsSlice = createSlice({
       state.comments = action.payload;
     },
     deletecomments: (state, action) => {
-      state.comments = state.comments.filter(comment => comment.id !== action.payload);
+      state.comments = state.comments.filter(
+        (comment) => comment.id !== action.payload
+      );
     },
     updateComment: (state, action) => {
-      const { id, updatedComment } = action.payload;
-      state.comments = state.comments.map(comment =>
-        comment.id === id ? { ...comment, ...updatedComment } : comment
+      const { id, Comment } = action.payload;
+      state.comments = state.comments.map((comment) =>
+        comment.id === id ? { ...comment, ...Comment } : comment
       );
     },
     getCommentById: (state, action) => {
       state.comments = [action.payload];
     },
-    setSingleComment: (state, action) => {
-      state.comments.push(action.payload);
+    createCommentLike: (state, action) => {
+      const newCommentLike = {
+  
+        commentId: action.payload.commentId,
+        userId: action.payload.userId,
+        
+      };
+      
+      state.commentLike.push(action.payload); 
+      return newCommentLike
+
     },
+    deleteCommentLikeById: (state, action) => {
+      state.commentLike = state.commentLike.filter(
+        (like) => like.id !== action.payload
+      ); 
+    },
+    getLikesByCommentId: (state, action) => {
+      
+      state.commentLike = state.commentLike.filter(
+        (like) => like.commentId === action.payload
+      );
+    },
+    getShareByPostId: (state, action) => {
+      state.shares = action.payload;
+    },
+    createShareByPostId: (state, action) => {
+      state.shares.push(action.payload);
+    },
+    softDeleteShare: (state, action) => {
+      state.shares = state.shares.filter(
+        (share) => share.id !== action.payload
+      );
+    },
+    
+    
   },
 });
-export const {setLoading, setError,
+export const {
+  setLoading,
+  setError,
   setcomments,
   getCommentsByPostId,
   deletecomments,
   updateComment,
-  getCommentById,setSingleComment
+  getCommentById,deleteCommentLikeByIdcreateCommentLike,getLikesByCommentId,getShareByPostId,
+  createShareByPostId,
+  softDeleteShare
 } = commentsSlice.actions;
 
 export default commentsSlice.reducer;
+//shares getShareByPostId, createShareByPostId, softDeleteShare
--- frontend/src/pages/comments/comments.jsx
+++ frontend/src/pages/comments/comments.jsx
@@ -1,6 +1,12 @@
 //import { useState, useEffect } from "react";
 //import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import { Container } from "@mui/material";
+import comment from'../../components/Post/Post'
+import Paper from "@mui/material/Paper";
+import { styled } from "@mui/material/styles";
+import Grid from "@mui/material/Grid";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import{ setLoading,
     setError,setcomments,
@@ -22,7 +28,8 @@ import{ setLoading,
       
       
         const comments = useSelector((state) => state.comments.comments);
-         const getCommentsByPostId = (postId) => async (dispatch) => {
+        useEffect(()=>{
+          const getCommentsByPostId = (postId) => async (dispatch) => {
             dispatch(setLoading());
             try {
               const response = await axios.get(`http://localhost:5000/comments/${postId}/comments`);
@@ -31,8 +38,9 @@ import{ setLoading,
               dispatch(setError(error.message));
             }
           };
-          
-           const deleteComment = (commentId) => async (dispatch) => {
+        })
+        useEffect(()=>{
+          const deleteComment = (commentId) => async (dispatch) => {
             try {
               await axios.delete(`http://localhost:5000/comments/${commentId}`);
               dispatch(deletecomments(commentId));
@@ -40,33 +48,42 @@ import{ setLoading,
               dispatch(setError(error.message));
             }
           };
+        })
           
-           const updateComment = (commentId, updatedComment) => async (dispatch) => {
-            try {
-              await axios.put(`http://localhost:5000/comments/${commentId}`, { comment: updatedComment });
-              dispatch(updateComment({ id: commentId, updatedComment }));
-            } catch (error) {
-              dispatch(setError(error.message));
-            }
-          };
+           useEffect(()=>{
+            const updateComment = (commentId, updatedComment) => async (dispatch) => {
+              try {
+                await axios.put(`http://localhost:5000/comments/${commentId}`, { comment: updatedComment });
+                dispatch(updateComment({ id: commentId, updatedComment }));
+              } catch (error) {
+                dispatch(setError(error.message));
+              }
+            };
+           })
           
-           const getCommentById = (commentId) => async (dispatch) => {
-            dispatch(setLoading());
-            try {
-              const response = await axios.get(`http://localhost:5000/comments/${commentId}`);
-              dispatch(setcomments(response.data.result)); 
-            } catch (error) {
-              dispatch(setError(error.message));
-            }
-          };
-          const deleteCommentLikeById = (likeId) => async (dispatch) => {
-            try {
-              await axios.delete(`http://localhost:5000/likes/${likeId}`);
-              dispatch(removeLike(likeId));
-            } catch (error) {
-              dispatch(setError(error.message));
-            }
-          };
+          
+          useEffect(()=>{
+            const getCommentById = (commentId) => async (dispatch) => {
+              dispatch(setLoading());
+              try {
+                const response = await axios.get(`http://localhost:5000/comments/${commentId}`);
+                dispatch(setcomments(response.data.result)); 
+              } catch (error) {
+                dispatch(setError(error.message));
+              }
+            };
+          })
+           useEffect(()=>{
+            const deleteCommentLikeById = (likeId) => async (dispatch) => {
+              try {
+                await axios.delete(`http://localhost:5000/likes/${likeId}`);
+                dispatch(removeLike(likeId));
+              } catch (error) {
+                dispatch(setError(error.message));
+              }
+            };
+           })
+         useEffect(()=>{
           const createCommentLike = (likeData) => async (dispatch) => {
             try {
               const response = await axios.post(`http://localhost:5000/likes`, likeData);
@@ -75,43 +92,53 @@ import{ setLoading,
               dispatch(setError(error.message));
             }
           };
-          const getLikesByCommentId = (commentId) => async (dispatch) => {
-            dispatch(setLoading());
-            try {
-              const response = await axios.get(`http://localhost:5000/comments/${commentId}/likes`);
-              dispatch(setLikes(response.data.result));
-            } catch (error) {
-              dispatch(setError(error.message));
-            }
-          };
-          const getShareByPostId = (postId) => async (dispatch) => {
-            dispatch(setLoading());
-            try {
-              const response = await axios.get(`http://localhost:5000/posts/${postId}/shares`);
-              dispatch(setShares(response.data.result));
-              dispatch(getShareByPostId())
-            } catch (error) {
-              dispatch(setError(error.message));
-            }
-          };
-          const createShareByPostId = (postId, shareData) => async (dispatch) => {
-            try {
-              const response = await axios.post(`http://localhost:5000/posts/${postId}/shares`, shareData);
-              dispatch(addShare(response.data.result));
-              dispatch(createShareByPostId());
-            } catch (error) {
-              dispatch(setError(error.message));
-            }
-          };
+         })
+          useEffect(()=>{
+            const getLikesByCommentId = (commentId) => async (dispatch) => {
+              dispatch(setLoading());
+              try {
+                const response = await axios.get(`http://localhost:5000/comments/${commentId}/likes`);
+                dispatch(setLikes(response.data.result));
+              } catch (error) {
+                dispatch(setError(error.message));
+              }
+            };
+          })
+          useEffect(()=>{
+            const getShareByPostId = (postId) => async (dispatch) => {
+              dispatch(setLoading());
+              try {
+                const response = await axios.get(`http://localhost:5000/posts/${postId}/shares`);
+                dispatch(setShares(response.data.result));
+                dispatch(getShareByPostId())
+              } catch (error) {
+                dispatch(setError(error.message));
+              }
+            };
+          })
+          useEffect(()=>{
+            const createShareByPostId = (postId, shareData) => async (dispatch) => {
+              try {
+                const response = await axios.post(`http://localhost:5000/posts/${postId}/shares`, shareData);
+                dispatch(addShare(response.data.result));
+                dispatch(createShareByPostId());
+              } catch (error) {
+                dispatch(setError(error.message));
+              }
+            };
+          })
           
-           const softDeleteShare = (shareId) => async (dispatch) => {
-            try {
-              await axios.put(`http://localhost:5000/shares/${shareId}`, { isDeleted: true });
-              dispatch(deleteShare(shareId))
-              dispatch(softDeleteShare())
-            } catch (error) {
-              dispatch(setError(error.message));
-            }
-        }
+          useEffect(()=>{
+            const softDeleteShare = (shareId) => async (dispatch) => {
+              try {
+                await axios.put(`http://localhost:5000/shares/${shareId}`, { isDeleted: true });
+                dispatch(deleteShare(shareId))
+                dispatch(softDeleteShare())
+              } catch (error) {
+                dispatch(setError(error.message));
+              }
+          }
+          })
+          
     }
     //export default commentsSlice.reducer;
