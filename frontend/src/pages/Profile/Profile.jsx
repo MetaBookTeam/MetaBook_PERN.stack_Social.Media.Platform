149216205b09a5cd372ebe0c5bfe49abb8c47686
RafatBSaqqa <raafat.saqqa@gmail.com>
2024-02-23 16:45:12 +0300

    add model

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -13,6 +13,11 @@ import Grid from "@mui/material/Grid";
 import AspectRatio from "@mui/joy/AspectRatio";
 // import Box from '@mui/joy/Box';
 import { styled } from "@mui/material/styles";
+import TextField from "@mui/material/TextField";
+import Backdrop from "@mui/material/Backdrop";
+import Modal from "@mui/material/Modal";
+import Fade from "@mui/material/Fade";
+import Typography from "@mui/material/Typography";
 const Profile = () => {
   const auth = useSelector((state) => state.auth);
   const [userProfile, setUserProfile] = useState([]);
@@ -23,6 +28,20 @@ const Profile = () => {
     textAlign: "center",
     color: theme.palette.text.secondary,
   }));
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+  };
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   const getUserById = async () => {
     try {
       const user = await axios.get(
@@ -46,7 +65,7 @@ const Profile = () => {
     <>
       {userProfile.user_name}
       {userProfile.email}
-      
+
       {userProfile.bio}
       <Container>
         <Grid container spacing={2}>
@@ -60,20 +79,56 @@ const Profile = () => {
                   />
                 </AspectRatio>
               </Box>
-              <Box sx={{ display: 'flex', gap: 2,justifyContent:"center" }}>
-              <Avatar
-                alt="Remy Sharp"
-                src={userProfile.image}
-                sx={{ width: 80, height: 80}}
-              />
-               </Box>
+              <Box sx={{ display: "flex", gap: 2, justifyContent: "center" }}>
+                <Avatar
+                  alt="Remy Sharp"
+                  src={userProfile.image}
+                  sx={{ width: 80, height: 80 }}
+                />
+              </Box>
             </Item>
           </Grid>
           <Grid item xs={4}>
             <Item>xs=4</Item>
           </Grid>
-          <Grid item xs={4}>
-            <Item>xs=4</Item>
+          <Grid item xs={8}>
+            <Item>
+              <h2>POSTS</h2>
+
+              <Button onClick={handleOpen}>What's on your mind</Button>
+              <Modal
+                aria-labelledby="transition-modal-title"
+                aria-describedby="transition-modal-description"
+                open={open}
+                onClose={handleClose}
+                closeAfterTransition
+                slots={{ backdrop: Backdrop }}
+                slotProps={{
+                  backdrop: {
+                    timeout: 500,
+                  },
+                }}
+              >
+                <Fade in={open}>
+                  <Box sx={style}>
+                    <Typography
+                      id="transition-modal-title"
+                      variant="h6"
+                      component="h2"
+                    >
+                      Text in a modal
+                    </Typography>
+                    <Typography
+                      id="transition-modal-description"
+                      sx={{ mt: 2 }}
+                    >
+                      Duis mollis, est non commodo luctus, nisi erat porttitor
+                      ligula.
+                    </Typography>
+                  </Box>
+                </Fade>
+              </Modal>
+            </Item>
           </Grid>
           <Grid item xs={8}>
             <Item>xs=8</Item>
