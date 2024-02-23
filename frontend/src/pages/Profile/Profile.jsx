7e5c6918e0344b6198cfc05c1bbb55458343792b
RafatBSaqqa <raafat.saqqa@gmail.com>
2024-02-23 16:18:58 +0300

    add style to profile page

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -1,44 +1,85 @@
 import { useDispatch, useSelector } from "react-redux";
 import * as React from "react";
-import { useEffect ,useState} from "react";
+import { useEffect, useState } from "react";
 import Avatar from "@mui/material/Avatar";
 import Stack from "@mui/material/Stack";
 import axios from "axios";
+import Container from "@mui/material/Container";
+import Card from "@mui/material/Card";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import AspectRatio from "@mui/joy/AspectRatio";
+// import Box from '@mui/joy/Box';
+import { styled } from "@mui/material/styles";
 const Profile = () => {
   const auth = useSelector((state) => state.auth);
   const [userProfile, setUserProfile] = useState([]);
-  
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  }));
   const getUserById = async () => {
-   try {
-    const user = await  axios.get(`http://localhost:5000/users/${auth.userId}`, {
-      headers: {
-        Authorization: `Bearer ${auth.token}`,
-      },
-    })
-    setUserProfile(...user.data.result);
-    console.log(user.data.result);
-   } catch (error) {
-    console.log(error);
-   }
-  }
+    try {
+      const user = await axios.get(
+        `http://localhost:5000/users/${auth.userId}`,
+        {
+          headers: {
+            Authorization: `Bearer ${auth.token}`,
+          },
+        }
+      );
+      setUserProfile(...user.data.result);
+      console.log(user.data.result);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   useEffect(() => {
     getUserById();
   }, []);
   return (
     <>
-     {userProfile.id}
-     {userProfile.image}
-     {userProfile.user_name}
-     {userProfile.email}
-     {userProfile.cover_photo}
-     {userProfile.bio}
-
-
-      <Avatar
-        alt="Remy Sharp"
-        src="/static/images/avatar/1.jpg"
-        sx={{ width: 70, height: 70 }}
-      />
+      {userProfile.user_name}
+      {userProfile.email}
+      
+      {userProfile.bio}
+      <Container>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Item>
+              <Box sx={{ borderRadius: "sm", p: 1 }}>
+                <AspectRatio minHeight={120} maxHeight={350}>
+                  <img
+                    src={userProfile.cover_photo}
+                    alt="A beautiful Cover photo."
+                  />
+                </AspectRatio>
+              </Box>
+              <Box sx={{ display: 'flex', gap: 2,justifyContent:"center" }}>
+              <Avatar
+                alt="Remy Sharp"
+                src={userProfile.image}
+                sx={{ width: 80, height: 80}}
+              />
+               </Box>
+            </Item>
+          </Grid>
+          <Grid item xs={4}>
+            <Item>xs=4</Item>
+          </Grid>
+          <Grid item xs={4}>
+            <Item>xs=4</Item>
+          </Grid>
+          <Grid item xs={8}>
+            <Item>xs=8</Item>
+          </Grid>
+        </Grid>
+      </Container>
     </>
   );
 };
