22539c09e15e46198cc2388938fcd2da14b32950
KamalLahloh <kamal.lahloh@gmail.com>
2024-02-25 05:02:27 +0300

    Registration Rules

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,7 @@ import "./App.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routers/routers";
 
-
 function App() {
-  
   return <RouterProvider router={router} />;
 }
 
