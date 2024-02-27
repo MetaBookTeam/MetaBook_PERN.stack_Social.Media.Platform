b3b8df6a99c0ebdc30bd67ef6c3f4dd2247c3b9d
RafatBSaqqa <raafat.saqqa@gmail.com>
2024-02-27 20:25:52 +0300

    add socket to frontend

---
+++ frontend/src/socket.server.js
@@ -0,0 +1,11 @@
+import io from 'socket.io-client'
+const socketInit = ({ user_id,token}) => {
+    return io('http://localhost:8080', {
+         extraHeaders: {
+             user_id,
+             token
+         },
+     } )
+ }
+ 
+ export default socketInit
