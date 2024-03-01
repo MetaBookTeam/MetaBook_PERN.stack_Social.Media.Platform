95d6ca7732ec9f7e40a9a7017a6728cdc5027f3a
nassarahmad <ahmad.m.nassarr@gmail.com>
2024-03-02 00:54:14 +0300

    about us done

--- frontend/src/pages/About/About.jsx
+++ frontend/src/pages/About/About.jsx
@@ -1,19 +1,62 @@
-import React from "react";
+import React from 'react';
+import Grid from '@mui/material/Grid';
 
-const About = () => {
-  return (
-    <div className="about">
-      <h1>Welcome to Our Social Media Community</h1>
-      <p>Join us in creating meaningful connections, sharing stories, and spreading positivity.</p>
-      <p>Discover a world of inspiration, creativity, and endless possibilities.</p>
-      <p>Connect with like-minded individuals, forge new friendships, and explore diverse perspectives.</p>
-      <p>Experience the joy of belonging to a vibrant online community where your voice matters.</p>
-      <p>Together, let's build a space where kindness, empathy, and respect thrive.</p>
-      <p>Join us today and be part of something truly special.</p>
-    </div>
-    
-  );
+const containerStyle = {
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: '100vh',
+  backgroundImage: "url(https://source.unsplash.com/random?wallpapers)",
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: 'cover',
 };
 
-export default About;
+const imageStyle = {
+  width: '100px',
+  height: '100px',
+  borderRadius: '50%',
+};
 
+const textStyle = {
+  textAlign: 'center',
+};
+const About = () => {
+  const linkStyle = {
+    textDecoration: 'none', // Remove underline decoration
+    color: 'inherit', // Inherit text color from parent
+  };
+
+  return (
+    <div style={containerStyle}>
+      <div style={{ ...textStyle, position: 'absolute', top: '20px', left: '50%', transform: 'translateX(-50%)' }}>
+        <h1>Welcome to My Social Media App</h1>
+      </div>
+      
+      <Grid container spacing={3} style={textStyle}>
+        <Grid item xs={3}>
+          <img src='/photos/kammal.jpeg' style={imageStyle} />
+          <p>Kammal Full-Stack web depeloper</p>
+          <p><a href='https://www.github.com' style={linkStyle}>Click!!</a></p>
+        </Grid>
+        <Grid item xs={3}>
+          <img src="/photos/saqqa.jpeg" alt="Saqqa" style={imageStyle} />
+          <p>Saqqa Full-Stack web depeloper</p>
+          <p><a href='https://www.github.com' style={linkStyle}>Click!!</a></p>
+        </Grid>
+        <Grid item xs={3}>
+          <img src="/photos/nassarr.jpeg" alt="Nassarr" style={imageStyle} />
+          <p>Nassarr Full-Stack web depeloper</p>
+          <p><a href='https://www.github.com' style={linkStyle}>Click!!</a></p>
+        </Grid>
+        <Grid item xs={3}>
+          <img src="/photos/osama.jpeg" alt="Osama" style={imageStyle} />
+          <p>Osama Full-Stack web depeloper</p>
+          <p><a href='https://www.github.com' style={linkStyle}>Click!!</a></p>
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
+
+export default About;
